cf3db3c7ffaa173c6eb73894ba8ffe316c59fe25
hoale0507 <hoale090293@gmail.com>
2021-12-08 22:39:47 +0700

    add file

---
+++ main.js
@@ -0,0 +1,24 @@
+let i = 10;
+let f = 20.5;
+let b = true;
+let s = "Hà Nội";
+document.write('i = ' + i)
+document.write('<br/>')
+document.write('f = ' + f)
+document.write('<br/>')
+document.write('b = ' + b)
+document.write('<br/>')
+document.write('s = ' + s)
+
+let width = 20
+let height = 10
+let area = width * height
+document.write('Area = ' + area)
+
+var a = prompt("Nhập giá trị của a");
+var b = prompt("Nhập giá trị của b");
+if (a%b == 0) {
+    alert("a chia hết cho b")
+} else {
+    alert("a không chia hết cho b")
+};
